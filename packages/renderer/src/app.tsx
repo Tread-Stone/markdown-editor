d9e8819f2a76bfb7859d290463720f4641c2317b
BrendanGlancy <bglance68@gmail.com>
2021-11-09 18:53:10 -0500

    markdown editor in progress

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -1,20 +1,19 @@
 import React, { useState, useCallback } from 'react'
+import Editor from './editor'
+import Preview from './preview'
 import './app.css'
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0)
+  const [doc, setDoc] = useState<string>('# Hello, World!')
 
+  const handleDoc = useCallback(newDoc => {
+    setDoc(newDoc)
+  }, [])
 
   return (
     <div className='app'>
-      <header>
-        <p>Hello + Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      </header>
+      <Editor onChange={handleDoc} initialDoc={doc} />
+      <Preview />
     </div>
   )
 }
