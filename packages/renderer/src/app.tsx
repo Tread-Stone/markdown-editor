88eaf7a86f03168d3845dc3bb24a53500045b426
brendanglancy <bglance68@gmail.com>
2021-12-06 17:11:22 -0500

    new example text

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -1,11 +1,18 @@
 import React, { useState, useCallback } from 'react'
 import Editor from './editor'
+import Fzf from './fuzz'
 import Preview from './preview'
 import './app.css'
 
 
 const App: React.FC = () => {
-  const [doc, setDoc] = useState<string>('# Hello, World!')
+  const [doc, setDoc] = useState<string>(`# Sorting and Seaching Arrays
+
+<b>Bubble Sort</b>
+- Useful for alphabetical list and numerical sorting
+- Array Elements are compared and numbers bubble toward the end of the array
+- Swapping elements must be done in order to properly sort the array
+`)
 
   const handleDoc = useCallback(newDoc => {
     setDoc(newDoc)
